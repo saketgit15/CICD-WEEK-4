230b8959073ad61f6be08e1e1d4c5f7ba4b96238
Kumar <saket.kumar@capgemini.com>
2025-09-07 11:47:22 +0530

    CI-CD-Demo first commit

---
+++ CI-CD-Demo/Dockerfile
@@ -0,0 +1,4 @@
+FROM openjdk:17-jdk-slim
+ADD spring-boot-docker.jar spring-boot-docker.jar
+EXPOSE 8081
+ENTRYPOINT ["java", "-jar", "spring-boot-docker.jar"]
---
+++ CI-CD-Demo/src/main/java/com/powerup/week4/CI_CD_Demo/CiCdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.powerup.week4.CI_CD_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdDemoApplication.class, args);
+	}
+
+}
